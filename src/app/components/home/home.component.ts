fe8fe74027dd2315e8d595f01fedb76c40b9f2ca
christopher <christopher.kade@epitech.eu>
2017-12-07 23:23:16 +0000

    User can now validate TODOs and delete them Updated some displays Minor tweaks

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -15,7 +15,7 @@ export class HomeComponent implements OnInit {
 
   // Adds a new todo to our list
   addTODO() {
-    this.listService.selectedList.todos.push(new Todo('New TODO', ''));
+    this.listService.selectedList.todos.unshift(new Todo('New TODO', ''));
   }
 
   // Save edited Todo
@@ -33,8 +33,18 @@ export class HomeComponent implements OnInit {
     }
   }
 
-  validateTODO(todo) {
+  // Validate a Todo and move it to the end of the list
+  validateTODO(todo, index) {
     // TODO: Update state locally
+    let element = this.listService.selectedList.todos[index];
+    this.listService.selectedList.todos.splice(index, 1);
+    this.listService.selectedList.todos.splice(this.listService.selectedList.todos.length, 0, element);
     todo.done = true;
+  }
+
+  // Delete a Todo
+  deleteTODO(index) {
+  // TODO: Delete them locally
+    this.listService.selectedList.todos.splice(index, 1);
   }
 }
